da6501fe83d658b0ef01d5a3ce9e32f84c6d9d69
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2023-05-11 13:50:30 +0300

    fix: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,16 +1,38 @@
+const fsPromises = require('fs/promises');
 const path = require('path');
-const fs = require('fs');
-const srcDirPath = path.join(__dirname, 'files');
-const distDirPath = path.join(__dirname, 'files-copy');
 
-fs.mkdir(distDirPath, { recursive: true }, (err) => {
-    if (err) throw err;
-    fs.readdir(srcDirPath, {withFileTypes: true}, function (err, files) {
-        if (err) {
-            return console.log('Unable to scan directory: ' + err);
-        } 
-        files.forEach((file) => {
-            fs.copyFile(path.join(srcDirPath,file.name),path.join(distDirPath,file.name),()=>{});
+async function copyFiles(distDirPath,srcDirPath) {
+    try {
+        await fsPromises.mkdir(distDirPath, {recursive: true});
+        const sourceFiles = await fsPromises.readdir(srcDirPath);
+        const destFiles = await fsPromises.readdir(distDirPath);
+        destFiles.forEach(async file => {
+          if (!sourceFiles.includes(file)) {
+            await fsPromises.unlink(path.join(distDirPath, file));
+          }
         });
-    });
-}); 
+        sourceFiles.forEach(async file => {
+          await fsPromises.copyFile(path.join(srcDirPath, file), path.join(distDirPath, file));
+        });
+      } 
+      catch (err) {
+        console.error(err);
+      }
+    
+}
+copyFiles(path.join(__dirname, 'files-copy'),path.join(__dirname, 'files'))
+
+
+// fs.mkdir(distDirPath, { recursive: true }, (err) => {
+//     if (err) throw err;
+
+//     fs.readdir(srcDirPath, {withFileTypes: true}, function (err, files) {
+//         if (err) {
+//             return console.log('Unable to scan directory: ' + err);
+//         } 
+
+//         files.forEach((file) => {
+//             fs.copyFile(path.join(srcDirPath,file.name),path.join(distDirPath,file.name),()=>{});
+//         });
+//     });
+// }); 
